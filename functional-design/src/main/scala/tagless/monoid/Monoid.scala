44da272bceee9ec6d47ffeb8602df79abf67b1ed
John Duffell <john.duffell@guardian.co.uk>
2022-04-11 15:59:41 +0100

    future interpreter

--- functional-design/src/main/scala/tagless/monoid/Monoid.scala
+++ functional-design/src/main/scala/tagless/monoid/Monoid.scala
@@ -1,6 +1,10 @@
 package tagless.monoid
 
-import cats.Monoid
+import cats.{Functor, Monad, Monoid}
+import cats.implicits.*
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 /*
  * Free Monoid finally tagless
@@ -32,13 +36,23 @@ given Letters[String] = new Letters[String] {
   override def n: String = "n"
 }
 
+given Letters[Future[String]] = new Letters[Future[String]] {
+  override def j: Future[String] = Future.successful("j")
+
+  override def o: Future[String] = Future.successful("o")
+
+  override def h: Future[String] = Future.successful("h")
+
+  override def n: Future[String] = Future.successful("n")
+}
+
 
 //*******************************
 // program
 //*******************************
 def program[A: Monoid]()(using letters: Letters[A]): (A, A) = {
   import letters.*
-  
+
   val combine = summon[Monoid[A]].combine _
   val result = combine(n, o)
 
@@ -54,18 +68,18 @@ def program[A: Monoid]()(using letters: Letters[A]): (A, A) = {
 // main
 //*******************************
 object Main extends App {
-
+  {
     val ex = program[String]()
     val (result, result2) = ex
     println("result: " + result)
     println("result2: " + result2)
+  }
+  {
+    given ExecutionContext = ExecutionContext.global
+    val ex = program[Future[String]]()
+    val (result, result2) = ex
+    println("result: " + Await.result(result, Duration.Inf))
+    println("result2: " + Await.result(result2, Duration.Inf))
+  }
 }
 
-
-
-
-// this is built in to monads, implemented as flatMap.
-given Monoid[String] = new Monoid[String] {
-  override def empty: String = ""
-  override def combine(x: String, y: String): String = x + y
-}
